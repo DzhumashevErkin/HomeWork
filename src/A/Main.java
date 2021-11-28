d34603f6a2526a9686f0e2a29eccb050eb865dd3
DzhumashevErkin <djumasheverkin@gmail.com>
2021-11-28 21:18:34 +0600

    13.2

---
+++ src/A/Main.java
@@ -0,0 +1,13 @@
+package A;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        st1.showStudentInfo();
+        Student st2 = new Student();
+        st2.showStudentInfo();
+        Student st3 = new Student();
+        st3.showStudentInfo();
+    }
+}
---
+++ src/B/Main.java
@@ -0,0 +1,11 @@
+package B;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.showInfo();
+        System.out.println();
+        Employee e1 = new Employee();
+        e1.showInfo();
+    }
+}
