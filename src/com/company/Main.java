c2bccf41f71ff1ebf722bb81697269a6ef2d6547
DzhumashevErkin <djumasheverkin@gmail.com>
2021-12-24 21:30:30 +0600

    DateBase v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Circle[] circles = new Circle[10];
+        for (int i = 1; i < circles.length + 1; i++) {
+            circles[i - 1] = new Circle(i);
+        }
+        int countCirclesMultipleOfThree = 0;
+        for (Circle circle : circles) {
+            if (circle.calculateSquare() % 3 == 0) {
+                countCirclesMultipleOfThree++;
+            }
+            System.out.println(circle.calculateSquare());
+        }
+        System.out.println(countCirclesMultipleOfThree);
+
     }
 }
