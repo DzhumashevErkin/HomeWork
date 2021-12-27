acd9ede53b66f5ba0a5a94588319f3a747b7e93b
DzhumashevErkin <djumasheverkin@gmail.com>
2021-12-27 15:26:30 +0600

    37 v2

--- src/HomeWork37/DateBase.java
+++ src/HomeWork37/DateBase.java
@@ -53,4 +53,74 @@ public class DateBase {
         return count;
     }
 
+    public static List<String> getTrenersWithSalary(){
+        String sql = "select t.full_name , st.salary \n" +
+                     "from treners t \n" +
+                     "join sport_types st on st.sport_type = t.sport_type";
+        List<String> list = new ArrayList<>();
+        try(Connection conn = connect();
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery(sql)){
+            while (rs.next()){
+                list.add(rs.getString("full_name") + " | " + rs.getDouble("salary"));
+            }
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return list;
+    }
+
+    public static double getAvgTrenersSalary(){
+        String sql = "select avg(st.salary)\n" +
+                "from treners t \n" +
+                "join sport_types st on st.sport_type = t.sport_type";
+        double avgTrenersSalary = 0;
+        try(Connection conn = connect();
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql)){
+            rs.next();
+            avgTrenersSalary = rs.getDouble("avg");
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return avgTrenersSalary;
+    }
+
+    public static List<String> getTrenersWithGoodIsTrener(){
+        String sql = "select full_name,\n" +
+                        "case \n" +
+                        "when length(full_name) > 3 then 'Молодец'\n" +
+                        "else 'Не молодец'\n" +
+                        "END\n" +
+                        "as is_good\n" +
+                        "from treners";
+        List<String> list = new ArrayList<>();
+        try(Connection conn = connect();
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql)){
+            while (rs.next()){
+                list.add(rs.getString("full_name") + " | " + rs.getString("is_good"));
+            }
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return list;
+    }
+
+    public static void getCountStudentsInGroup(){
+        String sql = "select g.code , count(s.id) \n" +
+                "from groupss g \n" +
+                "inner join students s on s.group_id = g.id \n" +
+                "group by g.code";
+        try(Connection conn = connect();
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery(sql)){
+            while (rs.next()){
+                System.out.println("В группе " + rs.getString("code") + " | " + rs.getInt("count") + " студентов");
+            }
+        }catch (SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
--- src/HomeWork37/Main.java
+++ src/HomeWork37/Main.java
@@ -1,8 +1,13 @@
 package HomeWork37;
 
+import java.util.Date;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(DateBase.getStudentsHavingChar());
-        System.out.println(DateBase.getTrainersCount());
+        System.out.println(DateBase.getTrenersWithSalary());
+        System.out.println(DateBase.getAvgTrenersSalary());
+        System.out.println(DateBase.getTrenersWithGoodIsTrener());
+        DateBase.getCountStudentsInGroup();
     }
 }
