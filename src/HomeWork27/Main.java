9f359f76741c80ea8fdfb0595a18184781c02ebb
DzhumashevErkin <myloveanimewki@gmail.com>
2022-02-01 21:47:45 +0600

    25

--- src/HomeWork27/Main.java
+++ src/HomeWork27/Main.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class    Main {
     public static void main(String[] args) throws IOException {
         Song song = new Song();
         song.setName("Hello");
