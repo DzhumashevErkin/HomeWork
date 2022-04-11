a116ea9e976e90ebd3e18629780e10dea9141c27
DzhumashevErkin <myloveanimewki@gmail.com>
2022-04-11 15:50:24 +0600

    25

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        Set<Integer> set = new TreeSet<>();
+        set.add(5);
+        set.add(50);
+        set.add(1);
+        set.add(2);
+        System.out.println(set);
     }
 }
